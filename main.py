7fde50dfc3bdee17fe5dcbcdbae7de5adbec6590
amine abouothmane <amineab203@gmail.com>
2020-02-14 20:06:17 +0100

    organize
    put some graph code into functions

--- main.py
+++ main.py
@@ -15,6 +15,68 @@ import seaborn as sns
 #for reference
 #https://towardsdatascience.com/a-data-scientists-perspective-on-the-wuhan-coronavirus-4d1110446478
 
+
+def number_of_confirmed_cases_over_time(data):
+    #plotting a bar chart of confirmed cases over time
+    sns.axes_style("whitegrid")
+    sns.barplot(
+            x="Date_date", 
+            y="Confirmed", data=data.groupby(['Date_date']).sum().reset_index(drop=None)
+            )
+    plt.xticks(rotation=60)
+    plt.ylabel('Number of confirmed cases',fontsize=15)
+    plt.xlabel('Dates',fontsize=15)
+    
+def Rate_of_death_vs_rate_of_recovery(data):
+    #plotting two line plots for deaths and recoveries respectively
+    plt.plot('Date_date', 'Deaths', data=data.groupby(['Date_date']).sum().reset_index(drop=None), color='red')
+    plt.plot('Date_date', 'Recovered', data=data.groupby(['Date_date']).sum().reset_index(drop=None), color='green')
+    plt.xticks(rotation=60)
+    plt.ylabel('Number of cases',fontsize=15)
+    plt.xlabel('Dates',fontsize=15)
+    plt.legend()
+    plt.show()
+    #               10 most affected countries, besides China
+
+def ten_most_affected_countries_besides_China(df_country):   
+    #We know that China is the most affected country by a large margin, #so lets create a bar plot to compare countries other than China
+    #increasing the figure size
+    plt.rcParams['figure.figsize']=(15,7)
+    sns.barplot(
+            x="Country",
+            y="Confirmed",
+            data=df_country[df_country.Country!='China'].nlargest(10,'Confirmed'),
+            palette=sns.cubehelix_palette(15, reverse=True)
+            )
+    plt.ylabel('Number of cases',fontsize=15)
+    plt.xlabel('Countries',fontsize=15)
+    plt.xticks(fontsize=13)
+    plt.yticks(fontsize=13)
+
+
+def A_closer_look_at_the_10_most_affected_provinces_in_China(data):
+    
+    #creating a separate dataframe for provinces
+    df_province=data[data['Country']=='China'].groupby(['Province/State']).max().reset_index(drop=None)
+    #selecting 10 most affected provinces
+    df_province=df_province.nlargest(10,'Confirmed')
+    df_province=df_province[['Province/State','Deaths','Recovered']]
+    #for multi-bar plots in seaborn, we need to melt the dataframe so #that the the deaths and recovered values are in the same column
+    df_province= df_province.melt(id_vars=['Province/State'])
+
+    sns.barplot(
+            x='Province/State', 
+            y='value', 
+            hue='variable', 
+            data=df_province
+            )
+    plt.xlabel('Provinces',fontsize=15)
+    plt.ylabel('Number of cases',fontsize=15)
+
+
+
+
+
 #reading the data from the csv
 data= pd.read_csv("data/2019_nCoV_data.csv")
 
@@ -60,84 +122,20 @@ in order to get the maximum number of reported cases for each country.
 
 #getting the total number of confirmed cases for each country
 df_country=data.groupby(['Country']).max().reset_index(drop=None)
-print(df_country[['Country','Confirmed','Deaths','Recovered']])
+#print(df_country[['Country','Confirmed','Deaths','Recovered']])
 
 #preparing data for a time-series analysis
 df_by_date=data.groupby(['Date_date']).sum().reset_index(drop=None)
 df_by_date['daily_cases']=df_by_date.Confirmed.diff()
 df_by_date['daily_deaths']=df_by_date.Deaths.diff()
 df_by_date['daily_recoveries']=df_by_date.Recovered.diff()
-print(df_by_date)
-
-#                   number of confirmed cases over time
+#print(df_by_date)
 
 
-#plotting a bar chart of confirmed cases over time
-sns.axes_style("whitegrid")
-sns.barplot(
-x="Date_date", 
-y="Confirmed", data=data.groupby(['Date_date']).sum().reset_index(drop=None)
-)
-plt.xticks(rotation=60)
-plt.ylabel('Number of confirmed cases',fontsize=15)
-plt.xlabel('Dates',fontsize=15)
-
-#               Rate of death vs rate of recovery
-
-#plotting two line plots for deaths and recoveries respectively
-plt.plot('Date_date', 'Deaths', data=data.groupby(['Date_date']).sum().reset_index(drop=None), color='red')
-plt.plot('Date_date', 'Recovered', data=data.groupby(['Date_date']).sum().reset_index(drop=None), color='green')
-plt.xticks(rotation=60)
-plt.ylabel('Number of cases',fontsize=15)
-plt.xlabel('Dates',fontsize=15)
-plt.legend()
-plt.show()
-#               10 most affected countries, besides China
-
-
-#We know that China is the most affected country by a large margin, #so lets create a bar plot to compare countries other than China
-#increasing the figure size
-plt.rcParams['figure.figsize']=(15,7)
-sns.barplot(
-x="Country",
-y="Confirmed",
-data=df_country[df_country.Country!='China'].nlargest(10,'Confirmed'),
-palette=sns.cubehelix_palette(15, reverse=True)
-)
-plt.ylabel('Number of cases',fontsize=15)
-plt.xlabel('Countries',fontsize=15)
-plt.xticks(fontsize=13)
-plt.yticks(fontsize=13)
-
-#                   A closer look at the 10 most affected provinces in China
-
-#creating a separate dataframe for provinces
-df_province=data[data['Country']=='China'].groupby(['Province/State']).max().reset_index(drop=None)
-#selecting 10 most affected provinces
-df_province=df_province.nlargest(10,'Confirmed')
-df_province=df_province[['Province/State','Deaths','Recovered']]
-#for multi-bar plots in seaborn, we need to melt the dataframe so #that the the deaths and recovered values are in the same column
-df_province= df_province.melt(id_vars=['Province/State'])
-
-sns.barplot(
-x='Province/State', 
-y='value', 
-hue='variable', 
-data=df_province
-)
-plt.xlabel('Provinces',fontsize=15)
-plt.ylabel('Number of cases',fontsize=15)
-
-
-
-
-
-
-
-
-
-
-
+#number_of_confirmed_cases_over_time(data)
+#Rate_of_death_vs_rate_of_recovery(data)
+#ten_most_affected_countries_besides_China(df_country)
+A_closer_look_at_the_10_most_affected_provinces_in_China(data)
 
 
 
